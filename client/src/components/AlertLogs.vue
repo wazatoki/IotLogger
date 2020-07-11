bcf23a8d8614aaf2df2079d990a3b0fc81b3472d
wazatoki <wazatoki@gmail.com>
2020-07-11 22:30:07 +0900

    アラート日付の書式を修正

--- client/src/components/AlertLogs.vue
+++ client/src/components/AlertLogs.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="alert-logs">
     <el-table :data="alertData" height="400" style="width: 100%" cell-class-name="alert-item">
-      <el-table-column prop="datetime" label="日時" width="160"></el-table-column>
+      <el-table-column prop="datetime" :formatter="dateformat" label="日時" width="160"></el-table-column>
       <el-table-column prop="category" label="カテゴリー" width="100"></el-table-column>
       <el-table-column prop="name" label="イベント情報"></el-table-column>
     </el-table>
@@ -13,6 +13,35 @@ export default {
   name: "AlertLogs",
   props: {
     alertData: Array
+  },
+  methods: {
+    dateformat: function(row, column, value){
+      const d = Date.parse(value);
+      if (!isNaN(d)) {
+        const dt = new Date(d);
+        const year = dt.getFullYear();
+        const month = dt.getMonth();
+        const day = dt.getDate();
+        const hours = dt.getHours();
+        const minutes = dt.getMinutes();
+        const seconds = dt.getSeconds();
+        return (
+          year +
+          "/" +
+          month +
+          "/" +
+          day +
+          " " +
+          hours +
+          ":" +
+          minutes +
+          ":" +
+          seconds
+        );
+      } else {
+        return "";
+      }
+    },
   }
 };
 </script>
