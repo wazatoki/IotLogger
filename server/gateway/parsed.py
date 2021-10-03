83219fa2da1753e3e7f8841810328a113b346375
wazatoki <wazatoki@gmail.com>
2021-10-03 00:23:35 +0000

    parsed_log取得時に一定間隔でデータを統合する

--- server/gateway/parsed.py
+++ server/gateway/parsed.py
@@ -4,6 +4,7 @@ from flask import jsonify
 
 from infrastructure import flaskSetup
 from repositories import parsed_log
+from services import parseCyclicData
 from util import util
 
 app = flaskSetup.app
@@ -13,7 +14,7 @@ def find_parsed_by_event_date():
     f = util.get_requested_from_datetime()
     t = util.get_requested_to_date()
     d = util.get_requested_selected_device()
-    items = parsed_log.find_by_event_date(f, t, d)
+    items = parseCyclicData.fetch_items(f, t, d)
     result_items = []
     for item in items:
         item.dt = timezone('UTC').localize(item.dt)
